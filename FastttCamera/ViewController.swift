5b64880ea0f143787f61e667c9b220617b8f0ad7
masuhara <info@masuhara.net>
2016-03-09 18:16:21 +0900

    Initial Commit

--- FastttCamera/ViewController.swift
+++ FastttCamera/ViewController.swift
@@ -8,18 +8,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FastttCameraDelegate {
+    
+    @IBOutlet weak var cameraView: UIView!
+    var camera: FastttFilterCamera = FastttFilterCamera()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        camera.filterImage = UIImage(named: "lookup")
+        camera.delegate = self
+        self.fastttAddChildViewController(camera)
+        camera.view.frame = cameraView.frame
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - FastttCameraDelegate
+    func cameraController(cameraController: FastttCameraInterface!, didFinishCapturingImage capturedImage: FastttCapturedImage!) {
+        
+    }
+    
+    func cameraController(cameraController: FastttCameraInterface!, didFinishScalingCapturedImage capturedImage: FastttCapturedImage!) {
+        
+    }
+    
+    func cameraController(cameraController: FastttCameraInterface!, didFinishNormalizingCapturedImage capturedImage: FastttCapturedImage!) {
+        
+    }
 
+    
+    // MARK: - Private
 
+    @IBAction private func takePhoto() {
+        camera.takePicture()
+    }
+    
+    @IBAction private func switchCamera() {
+        if FastttCamera.isCameraDeviceAvailable(FastttCameraDevice.Front) {
+            if camera.cameraDevice == FastttCameraDevice.Front {
+                camera.cameraDevice = FastttCameraDevice.Rear
+            }else {
+                camera.cameraDevice = FastttCameraDevice.Front
+            }
+        }
+    }
+    
+    @IBAction private func flash() {
+        if FastttCamera.isTorchAvailableForCameraDevice(camera.cameraDevice) {
+            if camera.cameraTorchMode == FastttCameraTorchMode.Off {
+                camera.cameraTorchMode = FastttCameraTorchMode.On
+            }else {
+                camera.cameraTorchMode = FastttCameraTorchMode.Off
+            }
+        }
+    }
 }
 
